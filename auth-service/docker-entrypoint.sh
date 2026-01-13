57b5e714fc1ed3260ffb927baf98f12e6923a60d
Tridibesh Nag <tnag97@gmail.com>
2026-01-13 22:09:50 +0530

    auth service deployment setup

--- auth-service/docker-entrypoint.sh
+++ auth-service/docker-entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 echo "Running database migrations..."
 npx prisma migrate deploy --schema ./src/prisma/schema.prisma
 
-echo "Starting API Gateway..."
+echo "Starting Auth Service..."
 exec node dist/src/main
--- auth-service/prisma.config.ts
+++ auth-service/prisma.config.ts
@@ -4,6 +4,6 @@ import { defineConfig, env } from 'prisma/config';
 export default defineConfig({
     schema: './src/prisma/schema.prisma',
     datasource: {
-        url: env('DIRECT_URL')
+        url: env('DATABASE_URL')
     }
 });
