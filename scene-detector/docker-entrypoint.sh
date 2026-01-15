5274251056d21c3e7382f4dd1e229b7736f8c06a
Tridibesh Nag <tnag97@gmail.com>
2026-01-15 16:37:59 +0530

    deploy scene detector

--- scene-detector/docker-entrypoint.sh
+++ scene-detector/docker-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Generating Prisma client..."
-python -m prisma generate --schema ./prisma/schema.prisma
-
 echo "Running database migrations..."
 python -m prisma migrate deploy --schema ./prisma/schema.prisma
 
