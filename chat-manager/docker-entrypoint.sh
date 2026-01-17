e97895b890948d2ddfe9200d51963da72b1fbea5
Tridibesh Nag <tnag97@gmail.com>
2026-01-17 17:18:44 +0530

    deploy chat manager

--- chat-manager/docker-entrypoint.sh
+++ chat-manager/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Generating Prisma client..."
+prisma generate --schema ./prisma/schema.prisma
+
 echo "Running database migrations..."
 prisma db push --schema ./prisma/schema.prisma --skip-generate
 
