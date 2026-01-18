3e24b79b1e416841b6c68e2c9b393cae06637ae6
Tridibesh Nag <tnag97@gmail.com>
2026-01-18 10:33:44 +0530

    deploy frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,14 @@ import { devtools } from '@tanstack/devtools-vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 const config = defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     devtools(),
     viteTsConfigPaths({
